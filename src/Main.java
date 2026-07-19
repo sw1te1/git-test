5a1cfd619a2c33df33088e1ec6bab57d33a0a47c
sw1te <jibaidog@163.com>
2026-07-19 14:05:57 +0800

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git2");
+
 
     }
 }
